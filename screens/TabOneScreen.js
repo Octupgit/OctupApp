1b7c905e23de27278420119360a01e75274204c2
Ben Zaad <ben@octup.com>
2023-03-20 14:23:22 +0200

    wip

--- screens/TabOneScreen.js
+++ screens/TabOneScreen.js
@@ -1,21 +1,38 @@
 import { StyleSheet } from "react-native";
+import styled from 'styled-components';
 
-import EditScreenInfo from "../components/EditScreenInfo";
-import { Text, View } from "../components/Themed";
+import ChartTest from "../components/EditScreenInfo";
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
+    <Container>
+      <Title>Ben</Title>
+      <Title>Tab One</Title>
+      <Seperator/>
+      <ChartTest path="/screens/TabOneScreen.tsx" />
+    </Container>
   );
 }
+
+const Container = styled.View`
+  flex: 1;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+  background-color: aquamarine;
+`
+
+const Title = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+`
+
+const Seperator = styled.View`
+  margin-vertical: 30%;
+  background-color: navy;
+  height: 1%;
+  width: 100%;
+`
 
 const styles = StyleSheet.create({
   container: {
@@ -23,10 +40,10 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
+  // title: {
+  //   fontSize: 20,
+  //   fontWeight: "bold",
+  // },
   separator: {
     marginVertical: 30,
     height: 1,
