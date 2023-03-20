08b2ba3fcf97fba873055f85900213bcdb7f1a2d
Ben Zaad <ben@octup.com>
2023-03-20 14:34:00 +0200

    styled components

--- screens/TabOneScreen.js
+++ screens/TabOneScreen.js
@@ -1,4 +1,3 @@
-import { StyleSheet } from "react-native";
 import styled from 'styled-components';
 
 import ChartTest from "../components/EditScreenInfo";
@@ -20,33 +19,17 @@ const Container = styled.View`
   align-items: center;
   justify-content: center;
   background-color: aquamarine;
-`
+`;
 
 const Title = styled.Text`
   font-size: 20px;
   font-weight: bold;
-`
+`;
 
 const Seperator = styled.View`
   margin-vertical: 30%;
   background-color: navy;
   height: 1%;
   width: 100%;
-`
+`;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  // title: {
-  //   fontSize: 20,
-  //   fontWeight: "bold",
-  // },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-});
